ad763c3505a36224eb72fd2e8674d4ff2822914f
obe <obezzanou@gmail.com>
2019-02-13 17:57:00 +0100

    zk reconnection

--- src/main/java/org/monitoring/oozie/kafka/producer/KafkaEventProducer.java
+++ src/main/java/org/monitoring/oozie/kafka/producer/KafkaEventProducer.java
@@ -25,6 +25,8 @@ import org.monitoring.oozie.zookeeper.FlowConfig;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.RemovalListener;
+import com.google.common.cache.RemovalNotification;
 import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
 
@@ -32,14 +34,22 @@ public class KafkaEventProducer {
 	private static final XLog LOGGER = XLog.getLog(KafkaEventProducer.class);
 	private static final Gson MAPPER = new Gson();
 	private static final SimpleDateFormat ISO_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
-	
+
 	public static final String KAFKA_CONFIG_PREFIX = "job.listener.kafka.config.";
 	public static final String EVENT_CONFIG_PREFIX = "job.listener.event.config.";
 	public static final String KAFKA_TOPIC_CONF = "job.listener.kafka.topic";
-	
-	private static final Cache<String, KafkaProducer<String, String>> CACHE = CacheBuilder.newBuilder()
-			.expireAfterAccess(1, TimeUnit.HOURS)
-			.build();
+
+	private static final Cache<String, KafkaProducer<String, String>> CACHE;
+
+	static {
+		CACHE = CacheBuilder.newBuilder().expireAfterAccess(1, TimeUnit.HOURS)
+				.removalListener(new RemovalListener<String, KafkaProducer<String, String>>() {
+					@Override
+					public void onRemoval(RemovalNotification<String, KafkaProducer<String, String>> notification) {
+						notification.getValue().close();
+					}
+				}).build();
+	}
 
 	private FlowConfig flowConfig;
 	private String bootstrapServers;
@@ -55,80 +65,74 @@ public class KafkaEventProducer {
 
 	private synchronized Optional<KafkaProducer<String, String>> getKafkaProducer(String jobName) {
 		KafkaProducer<String, String> producer = CACHE.getIfPresent(jobName);
-		return Optional.ofNullable(Optional.ofNullable(producer)
-			.orElseGet(() -> {
-				LOGGER.info("Init kafka producer");
-				Optional<Properties> props = Optional.ofNullable(bootstrapServers)
-					.map(bs -> {
-						Properties properties = createJobKafkaProducerProps(jobName).orElseGet(Properties::new);
-						properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bs);
-						properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-						properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-						return properties;
-					});
-				return props.map(p -> {
-					KafkaProducer<String, String> newProducer = new KafkaProducer<String, String>(p);
-					CACHE.put(jobName, newProducer);
-					return newProducer;
-				}).orElse(null);
-			}));
+		return Optional.ofNullable(Optional.ofNullable(producer).orElseGet(() -> {
+			LOGGER.info("Init kafka producer");
+			Optional<Properties> props = Optional.ofNullable(bootstrapServers).map(bs -> {
+				Properties properties = createJobKafkaProducerProps(jobName).orElseGet(Properties::new);
+				properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bs);
+				properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+				properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+				return properties;
+			});
+			return props.map(p -> {
+				KafkaProducer<String, String> newProducer = new KafkaProducer<String, String>(p);
+				CACHE.put(jobName, newProducer);
+				return newProducer;
+			}).orElse(null);
+		}));
 	}
-	
+
 	public Optional<Properties> createJobKafkaProducerProps(String jobName) {
 		Optional<Properties> jobProps = flowConfig.getEventConfiguration(jobName);
 		return jobProps.map(p -> p.keySet().stream().map(Object::toString))
-			.map(propsStream -> propsStream.filter(prop -> prop.indexOf(KAFKA_CONFIG_PREFIX) == 0))
-			.map(kafkaPropsStream -> kafkaPropsStream.collect(Collectors.toMap(
-					propKey -> propKey.toString().replaceAll(KAFKA_CONFIG_PREFIX, StringUtils.EMPTY), 
-					propKey -> jobProps.get().get(propKey))))
-			.map(map -> {
-				Properties p = new Properties();
-				p.putAll(map);
-				return p;
-			});
+				.map(propsStream -> propsStream.filter(prop -> prop.indexOf(KAFKA_CONFIG_PREFIX) == 0))
+				.map(kafkaPropsStream -> kafkaPropsStream.collect(Collectors.toMap(
+						propKey -> propKey.toString().replaceAll(KAFKA_CONFIG_PREFIX, StringUtils.EMPTY),
+						propKey -> jobProps.get().get(propKey))))
+				.map(map -> {
+					Properties p = new Properties();
+					p.putAll(map);
+					return p;
+				});
 	}
 
 	public void sendEvent(MonitoringEvent event) {
-		flowConfig.getEventConfiguration(event.getJobName())
-			.map(props -> eventToRecord(event, props))
-			.flatMap(record -> produceToKafka(event.getJobName(), record))
-			.flatMap(this::getMetadata)
-			.ifPresent(meta -> {
-				if (LOGGER.isDebugEnabled()) {
-					LOGGER.debug("Event sent to : " + meta.toString());
-				}
-			});
+		flowConfig.getEventConfiguration(event.getJobName()).map(props -> eventToRecord(event, props))
+				.flatMap(record -> produceToKafka(event.getJobName(), record)).flatMap(this::getMetadata)
+				.ifPresent(meta -> {
+					if (LOGGER.isDebugEnabled()) {
+						LOGGER.debug("Event sent to : " + meta.toString());
+					}
+				});
 	}
 
 	private ProducerRecord<String, String> eventToRecord(MonitoringEvent event, Properties props) {
 		String topic = props.getProperty(KAFKA_TOPIC_CONF);
 		MonitoringEvent newEvent = updateEventConfig(event, props);
-		
+
 		Map<String, Object> log = new HashMap<>();
-		
+
 		log.put("message", newEvent);
 		log.put("date", ISO_DATE_FORMAT.format(System.currentTimeMillis()));
-		
+
 		String jsonEvent = MAPPER.toJson(log);
 		return new ProducerRecord<String, String>(topic, jsonEvent);
 	}
-	
+
 	private MonitoringEvent updateEventConfig(MonitoringEvent event, Properties jobConfig) {
 		Map<String, Object> eventProps = jobConfig.keySet().stream().map(Object::toString)
-			.filter(key -> key.indexOf(EVENT_CONFIG_PREFIX) == 0)
-			.collect(Collectors.toMap(
-					key -> key.toString().toLowerCase().replace(EVENT_CONFIG_PREFIX, StringUtils.EMPTY), 
-					key -> jobConfig.get(key)));
-		
-		Arrays.stream(event.getClass().getDeclaredFields())
-			.map(f -> Pair.of(f, getFieldName(f)))
-			.filter(t -> eventProps.get(t.getRight()) != null)
-			.map(t -> Pair.of(t.getLeft(), eventProps.get(t.getRight())))
-			.forEach(t -> {
-				Object value = t.getRight();
-				Field field = t.getLeft();
-				populateField(event, field, value);
-			});
+				.filter(key -> key.indexOf(EVENT_CONFIG_PREFIX) == 0)
+				.collect(Collectors.toMap(
+						key -> key.toString().toLowerCase().replace(EVENT_CONFIG_PREFIX, StringUtils.EMPTY),
+						key -> jobConfig.get(key)));
+
+		Arrays.stream(event.getClass().getDeclaredFields()).map(f -> Pair.of(f, getFieldName(f)))
+				.filter(t -> eventProps.get(t.getRight()) != null)
+				.map(t -> Pair.of(t.getLeft(), eventProps.get(t.getRight()))).forEach(t -> {
+					Object value = t.getRight();
+					Field field = t.getLeft();
+					populateField(event, field, value);
+				});
 		return event;
 	}
 
@@ -141,19 +145,17 @@ public class KafkaEventProducer {
 		}
 		field.setAccessible(false);
 	}
-	
+
 	private String getFieldName(Field field) {
-		return Arrays.stream(field.getDeclaredAnnotationsByType(SerializedName.class))
-			.findFirst().map(SerializedName::value)
-			.map(String::toLowerCase)
-			.orElse(field.getName());
+		return Arrays.stream(field.getDeclaredAnnotationsByType(SerializedName.class)).findFirst()
+				.map(SerializedName::value).map(String::toLowerCase).orElse(field.getName());
 	}
-	
-	private Optional<Future<RecordMetadata>> produceToKafka(String jobName, ProducerRecord<String, String> record){
+
+	private Optional<Future<RecordMetadata>> produceToKafka(String jobName, ProducerRecord<String, String> record) {
 		return getKafkaProducer(jobName).map(p -> p.send(record));
 	}
-	
-	private Optional<RecordMetadata> getMetadata(Future<RecordMetadata> future){
+
+	private Optional<RecordMetadata> getMetadata(Future<RecordMetadata> future) {
 		try {
 			return Optional.of(future.get());
 		} catch (InterruptedException | ExecutionException e) {
--- src/main/java/org/monitoring/oozie/zookeeper/FlowConfig.java
+++ src/main/java/org/monitoring/oozie/zookeeper/FlowConfig.java
@@ -23,10 +23,13 @@ public class FlowConfig {
 	private static final XLog LOGGER = XLog.getLog(FlowConfig.class);
 	private static final int TIME_OUT = 10 * 1000;
 	private static final String JOB_NAME = "job_name";
-	private static final Cache<String, Properties> CACHE = CacheBuilder.newBuilder()
-			.expireAfterAccess(1, TimeUnit.HOURS)
-			.build();
-	private ZooKeeper zookeeper;
+	private static final Cache<String, Optional<Properties>> CACHE;
+	
+	static {
+		CACHE = CacheBuilder.newBuilder()
+				.expireAfterAccess(1, TimeUnit.HOURS)
+				.build();
+	}
 	
 	private String pathTemplate;
 	private String zookeeperServer;
@@ -34,18 +37,15 @@ public class FlowConfig {
 	public FlowConfig(String zookeeperServer, String zkPathPrefix) {
 		this.zookeeperServer = zookeeperServer;
 		pathTemplate = zkPathPrefix + "/$" + JOB_NAME + "$";
-		if(this.zookeeperServer != null) {
-			initZooKeeper();
-		}
 	}
 	
-	private void initZooKeeper() {
+	private Optional<ZooKeeper> initZooKeeperClient() {
 		LOGGER.info("Init zookeeper client");
-		zookeeper = Optional.ofNullable(zookeeper)
-			.orElseGet(() -> {
+		return Optional.ofNullable(zookeeperServer)
+			.map(srv -> {
 				final CountDownLatch connectionLatch = new CountDownLatch(1);
 				try {
-					ZooKeeper zk = new ZooKeeper(zookeeperServer, TIME_OUT, new Watcher() {
+					ZooKeeper zk = new ZooKeeper(srv, TIME_OUT, new Watcher() {
 						@Override
 						public void process(WatchedEvent event) {
 							if(event.getState() == KeeperState.SyncConnected) {
@@ -57,30 +57,32 @@ public class FlowConfig {
 					if(!zkInitiated) {
 						throw new IOException("Zk Init failed");
 					}
+					LOGGER.info("Zookeeper client Created");
 					return zk;
 				} catch (IOException | InterruptedException e) {
 					LOGGER.error(e);
 					return null;
 				}
 			});
-		LOGGER.info("Zookeeper client Created");
 	}
 	
 	public synchronized Optional<Properties> getEventConfiguration(String jobName) {
-		Properties props = CACHE.getIfPresent(jobName);
-		Optional<Properties> properties = Optional.ofNullable(Optional.ofNullable(props)
-			.orElseGet(() -> {
-				Optional<String> path = getConfigurationPath(jobName);
-				return Optional.ofNullable(zookeeper)
-					.flatMap(zk -> path.flatMap(p -> getDataFromZk(zk, p)))
-					.map(ByteArrayInputStream::new)
-					.flatMap(stream -> loadProperties(jobName, stream)).orElse(null);
-			}));
-		properties.ifPresent(p -> CACHE.put(jobName, p));
+		Optional<Properties> properties = CACHE.getIfPresent(jobName);
+		if(properties == null) {
+			properties = getConfigurationPath(jobName).flatMap(this::getPropertiesByPath);
+		}
+		CACHE.put(jobName, properties);
 		return properties;
 	}
 	
-	private Optional<Properties> loadProperties(String jobName, InputStream stream){
+	private Optional<Properties> getPropertiesByPath(String path){
+		return initZooKeeperClient()
+			.flatMap(zk -> getDataFromZk(zk, path))
+			.map(ByteArrayInputStream::new)
+			.flatMap(stream -> loadProperties(stream));
+	}
+	
+	private Optional<Properties> loadProperties(InputStream stream){
 		try {
 			Properties props = new Properties();
 			props.load(stream);
